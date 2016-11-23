759bd50930551376b5fff222d787882fa45e310a
Rajani <rajani.treddy@gmail.com>
2016-11-22 18:26:01 -0600

    javascripts pdt

---
+++ javascript/11_22_2016/rajani/pdt.js
@@ -0,0 +1,40 @@
+// variables-number
+
+
+var a =10;
+var id=5;
+
+
+//strings
+
+var address ='fremont';
+var y ='abcd';
+
+
+//null
+
+var name =null;
+
+
+//Boolean
+
+var isFormed =true;
+var isSelected =false;
+
+//undefined
+
+var company = undefined;
+
+
+//outputting in console
+
+console.log(a);
+console.log(id);
+console.log(address);
+console.log(y);
+console.log(name);
+console.log(isFormed);
+console.log(isSelected);
+console.log(company);
+
+
